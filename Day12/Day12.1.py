518bdc53d898468efff1f7986442f46d533b628d
Tobifrinator <friese.tobias99@gmail.com>
2020-12-12 13:07:39 +0100

    finished Day12

--- Day12/Day12.1.py
+++ Day12/Day12.1.py
@@ -1,7 +1,34 @@
 def main():
 	data=open('data.txt','r')
-	print(data.read())
-
+	movements=data.read().split()
+	#North South East and West are now polar coordiantes
+	cardinal={
+		"N":1j,
+		"S":-1j,
+		"E":1,
+		"W":-1
+	}
+	# R-Rotation changes East to South -> -1j,L-Rotation changes East to North -> +1j
+	rotation={
+		"R":-1j,
+		"L":1j
+	}
+	facing=1
+	manhattan_distance=0+0j
+	for movement in movements:
+		#move in the given direction
+		if movement[0] in "NSEW":
+			manhattan_distance=manhattan_distance+int(movement[1:])*cardinal[movement[0]]
+		#turn n*90 degrees, -> rotation ** n
+		if movement[0] in "LR":
+			facing=facing*(rotation[movement[0]]**(int(movement[1:])/90))
+		#move forward in the facing direction
+		if movement[0] in "F":
+			manhattan_distance=manhattan_distance+facing*int(movement[1:])
+	NS=manhattan_distance.imag
+	EW=manhattan_distance.real
+	#manhattan_distance=|NS|+|EW|
+	print("The ship moved",NS,"towards north and ",EW," towards east. The manhattan distance is", abs(NS)+abs(EW))
 
 if __name__ == "__main__":
 	main()
--- Day12/Day12.2.py
+++ Day12/Day12.2.py
@@ -1,7 +1,37 @@
 def main():
 	data=open('data.txt','r')
-	print(data.read())
+	movements=data.read().split()
+	#North South East and West are now polar coordiantes, NS are imaginary, EW are real
+	cardinal={
+		"N":1j,
+		"S":-1j,
+		"E":1,
+		"W":-1
+	}
+	# R-Rotation changes East to South -> -1j,L-Rotation changes East to North -> +1j
+	rotation={
+		"R":-1j,
+		"L":1j
+	}
+	#starting waypoint at 1N10E
+	waypoint=1j+10
+	manhattan_distance=0+0j
 
+	for movement in movements:
+		#move waypoint in the given direction
+		if movement[0] in "NSEW":
+			waypoint=waypoint+int(movement[1:])*cardinal[movement[0]]
+		#rotate waypoint n*90 degrees around the ship -> rotation ** n, R:clockwise, L:counterclockwise
+		if movement[0] in "LR":
+			waypoint=waypoint*(rotation[movement[0]]**(int(movement[1:])/90))
+		#move forward to the waypoint
+		if movement[0] in "F":
+			manhattan_distance=manhattan_distance+waypoint*int(movement[1:])
+
+	NS=manhattan_distance.imag
+	EW=manhattan_distance.real
+	#manhattan_distance=|NS|+|EW|
+	print("The ship moved",NS,"towards north and ",EW," towards east. The manhattan distance is", abs(NS)+abs(EW))
 
 if __name__ == "__main__":
 	main()
