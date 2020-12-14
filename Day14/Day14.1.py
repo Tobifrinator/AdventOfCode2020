d7913f420eaa1bae53b64e54cf751df28d7db37c
Tobifrinator <friese.tobias99@gmail.com>
2020-12-14 17:41:06 +0100

    finished Day14

--- Day14/Day14.1.py
+++ Day14/Day14.1.py
@@ -1,7 +1,31 @@
 def main():
 	data=open('data.txt','r')
-	print(data.read())
+	lines=data.read().split('\n')
+	destinations, sources=[], []
+	#no mask applied yet; mem is a dictionary so that we can take 'mem[index]' as our key and not have a bloated array, since many fields in mem aren't used
+	mask=''
+	mem={}
+	for line in lines:
+		destinations=destinations+[line.split(' = ')[0]]
+		sources=sources+[line.split(' = ')[1]]
 
+	for i,destination in enumerate(destinations):
+		#a new mask is applied from this point on
+		if destination=='mask':
+			mask=sources[i]
+		if "mem" in destination:
+			#apply the mask to the value of this line
+			for j,char in enumerate(mask):
+				if char=='X':
+					pass
+				if char=='1':
+					# put 1 at corresponding bit
+					sources[i]=str(int(sources[i]) | 1<<(len(mask)-1-j))
+				if char=='0':
+					# put 0 at corresponding bit
+					sources[i]=str(int(sources[i]) & (0xFFFFFFFFF^1<<(len(mask)-1-j)))
+			mem[destination]=int(sources[i])
+	print("The sum of all values left in memory is",sum(mem.values()))
 
 if __name__ == "__main__":
 	main()
--- Day14/Day14.2.py
+++ Day14/Day14.2.py
@@ -1,7 +1,35 @@
+def apply_mask(destination,mask):
+	# every bit where mask=1 has to become 1 -> OR, replace x by 0 so that it doesnt interfere
+	destination=destination | int(mask.replace('X','0'), base=2)
+	for j,char in enumerate(mask):
+		if char=='X':
+			# for every x, return the results if the bit was flipped and if it wasnt <-> if destination bit is 1 and if it is 0
+			return apply_mask(destination^(1<<(len(mask)-1-j)), mask[:j]+'0'+mask[j+1:]) + apply_mask(destination, mask[:j]+'0'+mask[j+1:])
+	return [destination]
+
+
 def main():
 	data=open('data.txt','r')
-	print(data.read())
+	lines=data.read().split('\n')
+	destinations, sources=[], []
+	#no mask applied yet; mem is a dictionary so that we can take 'mem[index]' as our key and not have a bloated array, since many fields in mem aren't used
+	mask=''
+	mem={}
+	for line in lines:
+		destinations=destinations+[line.split(' = ')[0]]
+		sources=sources+[line.split(' = ')[1]]
 
+	for i,destination in enumerate(destinations):
+		#a new mask is applied from this point on
+		if destination=='mask':
+			mask=sources[i]
+		if "mem" in destination:
+			#apply the mask to the destination of this line
+			for masked_destination in apply_mask(int(destination[4:-1]),mask):
+				mem[masked_destination]=int(sources[i])
+
+	print(mem)
+	print("The sum of all values left in memory is",sum(mem.values()))
 
 if __name__ == "__main__":
 	main()
