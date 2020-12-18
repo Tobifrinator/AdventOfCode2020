523bcd868325aba677e2ddd6f5d30174b2d7d0c7
Tobifrinator <friese.tobias99@gmail.com>
2020-12-18 19:22:03 +0100

    finished Day 18

--- Day18/Day18.1.py
+++ Day18/Day18.1.py
@@ -1,7 +1,37 @@
+import re
+
+def handle_parantheses(formula):
+	deepest_parantheses=re.findall(r'\(\d+ [+* \d]* \d+\)',formula)
+	if deepest_parantheses==[]:
+		return formula
+
+	for deep_par in deepest_parantheses:
+		#print(deep_par)
+		formula=formula.replace(deep_par, calculate_left_to_right(deep_par[1:-1]))
+	formula=handle_parantheses(formula)
+	return formula
+
+def calculate_left_to_right(formula):
+	calculation=formula.split()
+	result=calculation[0]
+	for i in range(1,len(calculation),2):
+		operation=result+calculation[i]+calculation[i+1]
+		result=str(eval(operation))
+	return(result)
+
+def calculate(formula):
+	formula=handle_parantheses(formula)
+	return int(calculate_left_to_right(formula))
+
+
 def main():
 	data=open('data.txt','r')
-	print(data.read())
-
+	formulas=data.read().split("\n")
+	sum=0
+	for formula in formulas:
+		formula=handle_parantheses(formula)
+		sum=sum+calculate(formula)
+	print("The sum of the result of all rows when disregarding multiply-before-add is",sum)
 
 if __name__ == "__main__":
 	main()
--- Day18/Day18.2.py
+++ Day18/Day18.2.py
@@ -1,7 +1,37 @@
+import re
+
+def handle_parantheses(formula):
+	deepest_parantheses=re.findall(r'\(\d+ [+* \d]* \d+\)',formula)
+	if deepest_parantheses==[]:
+		return formula
+
+	for deep_par in deepest_parantheses:
+		#print(deep_par)
+		formula=formula.replace(deep_par, plus_before_multiply(deep_par[1:-1]))
+	formula=handle_parantheses(formula)
+	return formula
+
+def plus_before_multiply(formula):
+	sums=formula.split(' * ')
+	formula=str(eval(sums[0]))
+	for sum in sums[1:]:
+		formula=formula+" * "+str(eval(sum))
+	return str(eval(formula))
+
+
+def calculate(formula):
+	formula=handle_parantheses(formula)
+	return int(plus_before_multiply(formula))
+
+
 def main():
 	data=open('data.txt','r')
-	print(data.read())
-
+	formulas=data.read().split("\n")
+	sum=0
+	for formula in formulas:
+		formula=handle_parantheses(formula)
+		sum=sum+calculate(formula)
+	print(sum)
 
 if __name__ == "__main__":
 	main()
