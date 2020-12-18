b09e55953cdeee70202857dedbaa621376e2f082
Tobifrinator <friese.tobias99@gmail.com>
2020-12-18 19:42:22 +0100

    Added comments to Day 18.1 and 18.2

--- Day18/Day18.1.py
+++ Day18/Day18.1.py
@@ -1,16 +1,22 @@
 import re
 
+"""
+calculate the content of the innermost parantheses and replace the parantheses with the result
+call it, until all parentheses levels are resolved and no parantheses remain
+"""
 def handle_parantheses(formula):
 	deepest_parantheses=re.findall(r'\(\d+ [+* \d]* \d+\)',formula)
 	if deepest_parantheses==[]:
 		return formula
 
 	for deep_par in deepest_parantheses:
-		#print(deep_par)
 		formula=formula.replace(deep_par, calculate_left_to_right(deep_par[1:-1]))
 	formula=handle_parantheses(formula)
 	return formula
 
+"""
+calculate from left-to-right, disregarding multiply-before-add
+"""
 def calculate_left_to_right(formula):
 	calculation=formula.split()
 	result=calculation[0]
@@ -19,6 +25,9 @@ def calculate_left_to_right(formula):
 		result=str(eval(operation))
 	return(result)
 
+"""
+calculate: resolve all parentheses, then calculate from left to right
+"""
 def calculate(formula):
 	formula=handle_parantheses(formula)
 	return int(calculate_left_to_right(formula))
@@ -28,6 +37,7 @@ def main():
 	data=open('data.txt','r')
 	formulas=data.read().split("\n")
 	sum=0
+	#calculate the result of every row and sum it up
 	for formula in formulas:
 		formula=handle_parantheses(formula)
 		sum=sum+calculate(formula)
--- Day18/Day18.2.py
+++ Day18/Day18.2.py
@@ -1,16 +1,23 @@
 import re
 
+
+"""
+calculate the content of the innermost parantheses and replace the parantheses with the result
+call it, until all parentheses levels are resolved and no parantheses remain
+"""
 def handle_parantheses(formula):
 	deepest_parantheses=re.findall(r'\(\d+ [+* \d]* \d+\)',formula)
 	if deepest_parantheses==[]:
 		return formula
 
 	for deep_par in deepest_parantheses:
-		#print(deep_par)
 		formula=formula.replace(deep_par, plus_before_multiply(deep_par[1:-1]))
 	formula=handle_parantheses(formula)
 	return formula
 
+"""
+calculate all subsums and multiply them: -> + before *
+"""
 def plus_before_multiply(formula):
 	sums=formula.split(' * ')
 	formula=str(eval(sums[0]))
@@ -18,7 +25,9 @@ def plus_before_multiply(formula):
 		formula=formula+" * "+str(eval(sum))
 	return str(eval(formula))
 
-
+"""
+calculate: resolve all parentheses, then calculate from left to right
+"""
 def calculate(formula):
 	formula=handle_parantheses(formula)
 	return int(plus_before_multiply(formula))
@@ -28,10 +37,11 @@ def main():
 	data=open('data.txt','r')
 	formulas=data.read().split("\n")
 	sum=0
+	#calculate the result of every row and sum it up
 	for formula in formulas:
 		formula=handle_parantheses(formula)
 		sum=sum+calculate(formula)
-	print(sum)
+	print("The sum of the result of all rows when using add-before-multiply is",sum)
 
 if __name__ == "__main__":
 	main()
